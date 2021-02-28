6ab81e3270f478dae2bde1048fffe98544d6273b
ivebencrazy <ivebencrazy@users.noreply.github.com>
2021-02-27 19:58:39 -0800

    Add scripts

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 deno bundle --config=tsconfig.json --no-check ./source/main.ts ./public/main.bundle.js
--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 deno bundle --unstable --watch --config=tsconfig.json ./source/main.ts ./public/main.bundle.js
